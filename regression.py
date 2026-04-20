6c26c027078f286ab34e06cb4cdebc6cdc73d579
James Baker <jlbaker361@gmail.com>
2026-04-19 23:54:41 -0400

    regress

---
+++ regression.py
@@ -0,0 +1,50 @@
+import numpy as np
+import argparse
+import os
+import csv
+import time
+from tqdm import tqdm
+
+
+parser=argparse.ArgumentParser()
+parser.add_argument("--y_column",type=str,default="aesthetic") #column 0 = aesthetic column = 1 = p(unsafe)
+parser.add_argument("--block",type=str,default="down_blocks.2.attentions.1")
+
+info_path="laion/info.csv"
+sparse_dir="sparse_embeddings"
+dest_dir="statistics"
+
+if __name__=="__main__":
+    args=parser.parse_args()
+    dependent=[]
+    independent=[]
+    with open(info_path,"r") as file:
+        for l,line in enumerate(tqdm(file.readlines())):
+            [imgpath,aesthetic,punsafe]=line.strip().split(",")
+            imgpath=imgpath.split("/")[1]
+            aesthetic=float(aesthetic)
+            punsafe=float(punsafe)
+            target={
+                "aesthetic":aesthetic,
+                "punsafe":punsafe
+            }[args.y_column]
+            npz_file=os.path.join(sparse_dir,imgpath)
+            features=np.load(npz_file)[args.block]
+            n=features.shape[0]
+            if l<10:
+                print(features.shape)
+            dependent.extend([target for _ in range(n)])
+            independent.extend([f for f in features])
+            
+    print(" len samples",len(dependent))
+    
+    independent=np.array(independent)
+    dependent=np.array(dependent)
+    t0=time.time()
+    x,residuals,rank,s=np.linalg.lstsq(independent,dependent,rcond=None)
+    print(f"lstsq: {time.time()-t0:.2f}s")
+    t0=time.time()
+    covariance=np.corrcoef(independent)
+    print(f"covariance: {time.time()-t0:.2f}s")
+    np.savez(os.path.join(dest_dir,args.block),weights=x,covar=covariance)
+    
