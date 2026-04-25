b351d12b194b2abeaed694e3203f8c46eac19ced
James Baker <jlbaker361@gmail.com>
2026-04-24 21:55:44 -0400

    regression

--- regression.py
+++ regression.py
@@ -5,6 +5,39 @@ import csv
 import time
 from tqdm import tqdm
 
+class ElasticRegression:
+    def __init__(self, learning_rate, iterations, l1_penalty, l2_penalty):
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.l1_penalty = l1_penalty
+        self.l2_penalty = l2_penalty
+
+    def fit(self, X, Y):
+        self.m, self.n = X.shape
+        self.W = np.zeros(self.n)
+        self.b = 0
+        self.X = X
+        self.Y = Y
+        for i in range(self.iterations):
+            self.update_weights()
+        return self
+
+    def update_weights(self):
+        Y_pred = self.predict(self.X)
+        dW = np.zeros(self.n)
+        for j in range(self.n):
+            l1_grad = self.l1_penalty if self.W[j] > 0 else -self.l1_penalty
+            dW[j] = (
+                -2 * (self.X[:, j]).dot(self.Y - Y_pred) +
+                l1_grad + 2 * self.l2_penalty * self.W[j]
+            ) / self.m
+        db = -2 * np.sum(self.Y - Y_pred) / self.m
+        self.W -= self.learning_rate * dW
+        self.b -= self.learning_rate * db
+        return self
+
+    def predict(self, X):
+        return X.dot(self.W) + self.b
 
 parser=argparse.ArgumentParser()
 parser.add_argument("--y_column",type=str,default="aesthetic") #column 0 = aesthetic column = 1 = p(unsafe)
