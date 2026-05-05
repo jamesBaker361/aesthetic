7843d991bbd94afe18a1c43229a90d4c77a0f0c6
James Baker <jlbaker361@gmail.com>
2026-05-04 22:15:38 -0400

    extwwcing

--- mapping.py
+++ mapping.py
@@ -12,7 +12,22 @@ clip_model = CLIPVisionModelWithProjection.from_pretrained("openai/clip-vit-larg
 processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-large-patch14")
 
 
+file_path = hf_hub_download(
+    repo_id="deepghs/nsfw_detect",
+    filename=f"nsfw_dataset_v1.zip",
+    repo_type="dataset"
+    )
 
+import zipfile
+from pathlib import Path
+
+zip_path = Path(file_path)
+target_dir = Path("path/to/extract")
+
+target_dir.mkdir(parents=True, exist_ok=True)
+
+with zipfile.ZipFile(zip_path, 'r') as z:
+    z.extractall(target_dir)
 
 import os
 
@@ -20,16 +35,19 @@ folder="smutfolder"
 
 os.makedirs(folder,exist_ok=True)
 
-for n in range(5):
-    file_path = hf_hub_download(
-    repo_id="wallstoneai/civitai-top-nsfw-images-with-metadata",
-    filename=f"images/{n}.jpeg",
-    repo_type="dataset"
-    )
-    img=Image.open(file_path).convert("RGB")
-    h,w =img.size
-    img=img.resize((h//4,w//4))
-    new_img=get_maps(img,nsfw_model,aesthetic_model,device,processor,clip_model)
-    new_img.save(f"{folder}/{n}.png")
+for n in range(10):
+    try:
+        file_path = hf_hub_download(
+        repo_id="wallstoneai/civitai-top-nsfw-images-with-metadata",
+        filename=f"images/{n}.jpeg",
+        repo_type="dataset"
+        )
+        img=Image.open(file_path).convert("RGB")
+        h,w =img.size
+        img=img.resize((h//4,w//4))
+        new_img=get_maps(img,nsfw_model,aesthetic_model,device,processor,clip_model)
+        new_img.save(f"{folder}/{n}.png")
+    except:
+        pass
     
     
