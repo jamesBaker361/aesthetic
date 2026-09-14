3a78e4b98efe25a1670d6daa9ae34e7471173962
James Baker <jlbaker361@gmail.com>
2026-09-14 15:08:00 -0400

    resize nsfw

--- resize.py
+++ resize.py
@@ -4,13 +4,15 @@ from PIL import Image
 
 image_processor=VaeImageProcessor()
 
-src_dir="real_fruit"
+src_dir="artificial_nsfw"
+
+size=512
 
 for file in os.listdir(src_dir):
     img=Image.open(f"{src_dir}/{file}")
     (h,w)=img.size
-    if h!=256 or w!=256:
-        scale= 256./float(max(h,w))
+    if h!=size or w!=size:
+        scale= float(size)/float(max(h,w))
         img = img.resize((int(scale*h),int(scale*w)))
     image_pt=image_processor.preprocess(img)
 
